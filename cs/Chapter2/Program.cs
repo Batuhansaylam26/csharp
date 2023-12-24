9cb28c8454935c087f697c213fe1460f5094fc31
Batuhansaylam26 <106963447+Batuhansaylam26@users.noreply.github.com>
2023-12-25 02:01:38 +0300

    Add files via upload

---
+++ cs/Chapter2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+class Program
+{
+    //there is no importance of location of main method
+    static void Main(string[] args)
+    {
+        Console.WriteLine(len());
+        Console.WriteLine(UpperCase());
+        Console.WriteLine(ReplaceString());
+    }
+    static int len()
+    {
+        string name = "Batuhan SAYLAM";
+        return name.Length;
+    }
+    static string UpperCase()
+    {
+        string name = "Batuhan SAYLAM";
+        return name.ToUpper();
+    }
+    static string ReplaceString()
+    {
+        string name = "Batuhan SAYLAM";
+        return name.Replace("Batuhan","Yafes");
+    }
+    /*
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello");
+            Console.WriteLine("World");
+            Console.WriteLine(3 + 4 + 5);
+            Console.WriteLine(account());
+            Console.WriteLine(account2());
+
+        }
+    */
+}
---
+++ cs/chapter1/chapter1/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+class Program
+{
+    //there is no importance of location of main method
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello");
+        Console.WriteLine("World");
+        Console.WriteLine(3 + 4 + 5);
+        Console.WriteLine(account());
+        Console.WriteLine(account2());
+        Console.WriteLine(len());
+
+    }
+    static float account()
+    {
+        float balance = 100000.00f;
+        float interest_rate = 0.05f;
+        for (int i = 1; i < 5; i++)
+        {
+            float interest = balance * interest_rate;
+            balance += interest;
+
+        }
+        return balance;
+    }
+
+    static double account2()
+    {
+        double balance = 100000.00;
+        double interest_rate = 0.05;
+        for (int i = 1; i < 5; i++)
+        {
+            double interest = balance * interest_rate;
+            balance += interest;
+
+        }
+        return balance;
+    }
+    static int len()
+    {
+        string name = "Batuhan SAYLAM";
+        return name.Length;
+    }
+/*
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello");
+        Console.WriteLine("World");
+        Console.WriteLine(3 + 4 + 5);
+        Console.WriteLine(account());
+        Console.WriteLine(account2());
+
+    }
+*/
+}
